202bd7b1d124db1b8333632616ceb091b27a2d12
KesterJ <kester.jarvis@gmail.com>
2024-12-02 09:02:05 +0000

    Day 2 both parts

--- day_2.py
+++ day_2.py
@@ -14,12 +14,20 @@ def check_safety(item1, item2, direction):
     else:
         return False
 
-def check_all_safety(list):
+def check_all_safety(list, tolerance):
     direction = get_direction(list)
     safe = True
     j = 0
-    while safe == True and j < (len(list) - 1):
-        safe = check_safety(list[j], list[j+1], direction)
+    current_errors = 0
+    while safe == True and j < (len(list) - 1) and current_errors <= tolerance:
+        safety = check_safety(list[j], list[j+1], direction)
+        if not safety:
+            if current_errors == tolerance:
+                safe = False
+            else:
+                current_errors += 1
+            if j < len(list) - 2 and check_safety(list[j], list[j+2], direction):
+                j += 1
         j += 1
     return(safe)
 
@@ -28,6 +36,15 @@ total_safety = 0
 with open("Inputs/Input day 2", "r") as input_file:
     for line in input_file:
         contents = [int(i) for i in line.split()]
-        total_safety += check_all_safety(contents)
+        total_safety += check_all_safety(contents, tolerance = 0)
 
 print(total_safety)
+
+#Part 2
+total_safety_2 = 0
+with open("Inputs/Input day 2", "r") as input_file:
+    for line in input_file:
+        contents = [int(i) for i in line.split()]
+        total_safety_2 += check_all_safety(contents, tolerance = 1)
+
+print(total_safety_2)
