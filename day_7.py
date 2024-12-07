510c37144edd4cffb0f3b1aa7f1189e0c06f161e
KesterJ <kester.jarvis@gmail.com>
2024-12-07 09:45:18 +0000

    Day 7 part 2

--- day_7.py
+++ day_7.py
@@ -1,19 +1,35 @@
-def carol(problem):
+def cat_numbers(num1, num2):
+    return(int(str(num1) + str(num2)))
+
+def carol(problem, concatenation = False):
     total = problem[0]
     numbers = problem[1]
     #Check once we have only two numbers
     if len(numbers) == 2:
-        if numbers[0] * numbers[1] == total or numbers[0] + numbers[1] == total:
+        if (numbers[0] * numbers[1] == total or
+            numbers[0] + numbers[1] == total or
+            (concatenation == True and cat_numbers(numbers[1], numbers[0]) == total)):
             return(True)
         else:
             return(False)
-    #Otherwise check factorisation and do appropriate checks
-    if total % numbers[0] != 0:
-        return carol([total - numbers[0], numbers[1:]])
+    #Check factorisation
+    if total % numbers[0] == 0:
+        with_multiply = carol([total // numbers[0], numbers[1:]], concatenation = concatenation)
     else:
-        result = (carol([total / numbers[0], numbers[1:]]) or
-                  carol([total - numbers[0], numbers[1:]]))
-        return result
+        with_multiply = False
+    #Check concatenation
+    total_str = str(total)
+    current_str = str(numbers[0])
+    if concatenation == True and len(total_str) > len(current_str) and total_str[-len(current_str):] == current_str:
+        with_concat = carol([int(total_str[:-len(current_str)]), numbers[1:]], concatenation = concatenation)
+    else:
+        with_concat = False
+    if numbers[0] < total:
+        with_add = carol([total - numbers[0], numbers[1:]], concatenation = concatenation)
+    else:
+        with_add = False
+    result = with_multiply or with_concat or with_add
+    return result
 
 with open('Inputs/Input day 7', 'r') as input_file:
     inputs = input_file.readlines()
@@ -26,3 +42,4 @@ for i in range(0, len(problems)):
     problems[i][1].reverse()
 
 answer = sum([problem[0] for problem in problems if carol(problem)])
+answer_2 = sum([problem[0] for problem in problems if carol(problem, concatenation = True)])
